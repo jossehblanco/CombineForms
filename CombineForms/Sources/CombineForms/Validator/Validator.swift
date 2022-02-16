dd6534605c6fa535cb180a2a95d0b163744d5e1b
jossehblanco <jossehblanco@gmail.com>
2022-02-16 12:51:15 -0600

    Improve project structure

---
+++ CombineForms/Sources/CombineForms/Validator/Validator.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Josseh Blanco on 14/2/22.
+//
+
+import Foundation
---
+++ ExampleApp/CombineFormsExample/CombineFormsExample/FormViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  FormViewModel.swift
+//  CombineFormsExample
+//
+//  Created by Josseh Blanco on 14/2/22.
+//
+
+import Foundation
