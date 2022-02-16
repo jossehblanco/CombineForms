242b15c25f0436c006fb27d18caa4391b62405fb
jossehblanco <jossehblanco@gmail.com>
2022-02-16 12:52:19 -0600

    Add working example to the example app

--- ExampleApp/CombineFormsExample/CombineFormsExample/ContentView.swift
+++ ExampleApp/CombineFormsExample/CombineFormsExample/ContentView.swift
@@ -6,11 +6,57 @@
 //
 
 import SwiftUI
+import CombineForms
 
 struct ContentView: View {
+    @ObservedObject var viewModel = FormViewModel()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            makeField(field: viewModel.$username, text: $viewModel.username)
+            makeField(field: viewModel.$email, text: $viewModel.email)
+            makeField(field: viewModel.$fullName, text: $viewModel.fullName)
+        }
+        
+        Button(action: {
+            
+        }, label: {
+            ZStack {
+                RoundedRectangle(cornerRadius: 5)
+                    .fill(!viewModel.formValid ? .gray : .blue)
+                    .frame(width: 120, height: 40)
+                Text("Submit")
+                    .foregroundColor(!viewModel.formValid ? .black : .white)
+            }
+        })
+            .disabled(!viewModel.formValid)
+        
+        VStack {
+            Text(viewModel.formErrors)
+                .foregroundColor(.red)
+        }
+        .padding()
+    }
+    
+    @ViewBuilder func makeField(field: CombineFormField, text: Binding<String>) -> some View {
+        HStack(spacing: 4) {
+
+            Spacer()
+            
+            Image(systemName: "checkmark.circle.fill")
+                .resizable()
+                .frame(width: 24, height: 24)
+                .foregroundColor(field.isValid ? .green : .red)
+            
+            TextField(field.label, text: text)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .padding(.all, 20)
+                .disableAutocorrection(true)
+                .autocapitalization(.none)
+                .padding()
+            Spacer()
+        }
     }
 }
 
--- ExampleApp/CombineFormsExample/CombineFormsExample/FormViewModel.swift
+++ ExampleApp/CombineFormsExample/CombineFormsExample/FormViewModel.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import Combine
+import CombineForms
+
+class FormViewModel: ObservableObject, CombineFormValidating {
+    
+    @Published var formValid: Bool = true
+    @Published var formErrors: String = ""
+    
+    @CombineFormField(configuration: .nonEmpty, label: "Username", type: .text)
+    var username = ""
+    
+    @CombineFormField(configuration: .email, label: "Email", type: .text, validator: DefaultValidator(errorStrategy: .highestPriority))
+    var email = ""
+    
+    @CombineFormField(configuration: .nonEmpty, label: "Full Name", type: .text, validator: DefaultValidator(errorStrategy: .override(message: "This error message has been overriden.")))
+    
+    var fullName = ""
+    
+    lazy var fields: [CombineFormField] = [$username, $email, $fullName]
+    
+    var cancellables: Set<AnyCancellable> = .init()
+    
+    init() {
+        activateForm()
+    }
+}
